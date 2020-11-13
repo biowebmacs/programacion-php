14c26bc163209edd52e89f0bfc4336460da7e817
casa\miguel <macs.consult.ccg@gmail.com>
2020-11-12 19:38:25 -0500

    cambio 2

--- index.php
+++ index.php
@@ -1,6 +1,34 @@
 <?php 
 echo "Mi Nombre Es Miguel Cano";
 
+// Constante
+const NOMBRE= "Javier" ;
+
+//Variable
+$mainAddress = "Canto Grande";
+
+$booleana = FALSE;
+echo gettype ($boolean)
+
+//string
+$nombrecompleto = "Miguel Angel Cano";
+
+//integer
+$numero = 15;
+
+//double
+$numerodecimal = 10.5;
+
+//arrays
+$frutas = ['manzana','platano','pera']
+
+
 
 ?>
-<h1> Cambio </h1>
+<html> 
+<body>
+<h1> Estoy en Clase de Programacion </h1>
+<p> <?php echo gettype($nombrecompleto)?> </p>
+</body>
+</html>
+
